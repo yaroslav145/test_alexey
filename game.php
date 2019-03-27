a4a7589ac997e046440a50859a6854c9f3ca1a8d
Yaroslav <yaroslav-bukatchuk@yandex.ru>
2019-03-27 23:37:55 +0300

    erg

--- game.php
+++ game.php
@@ -1,3 +1,15 @@
+<form action="game.php">
+	<p><input name="0" type="submit" value="^"></p>
+	<p>
+		<input name="1" type="submit" value="<">
+		<input name="2" type="submit" value=">">
+	</p>
+	<p><input name="3" type="submit" value="V"></p>
+</form>
+
+
 <?php
+	print_r($_GET);
+	
 	
 ?>
