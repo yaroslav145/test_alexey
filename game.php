36b718203b962e140f5aeb537dbe8240a5672ef9
Yaroslav <yaroslav-bukatchuk@yandex.ru>
2019-03-31 02:31:26 +0300

    add multiplayer

--- game.php
+++ game.php
@@ -1,14 +1,22 @@
 <?php
+	$name = $_GET['name'];
+
+	$global_session_id = 9;
+	
+	//$mysession = session_id();
+	session_id($global_session_id);
 	session_start();
+	$temp_session = $_SESSION;
+	session_write_close();
 
 	//print_r($_GET);
 		
 	$step = 30;
 	$yaw = 0;
 		
-	if(!isset($_SESSION['player']))
+	if(!isset($temp_session[$name]))
 	{
-		$_SESSION['player'] = array(
+		$temp_session[$name] = array(
 			'yaw' => 0,
 		
 			0 => array(
@@ -17,14 +25,14 @@
 			),
 		);
 		
-		$_SESSION['ball'] = array(
+		$temp_session['ball'] = array(
 			'x' => 30 * 3,
 			'y' => 30 * 2,
 		);
 	}
 	
-	$ball = $_SESSION['ball'];
-	$player = $_SESSION['player'];
+	$ball = $temp_session['ball'];
+	$player = $temp_session[$name];
 	
 	
 	if(isset($_GET['0']))
@@ -44,7 +52,6 @@
 	
 	if(($ball['x'] == $player[0]['x']) && ($ball['y'] == $player[0]['y']))
 	{
-		
 		$ball['x'] = rand(0, 1600/$step) * $step;
 		$ball['y'] = rand(0, 700/$step) * $step;
 		
@@ -76,42 +83,62 @@
 			$prev = $temp;
 		}
 	}
-?>
+
+	
+	$temp_session['ball'] = $ball;
+	$temp_session[$name] = $player;
 
 
-<form name="w">
-	<input type="hidden" name="0">
-</form>
+	echo '<form name="w">
+		<input type="hidden" name="0">
+		<input type="hidden" name="name" value='.$name.'>
+	</form>
 
-<form name="a">
-	<input type="hidden" name="1">
-</form>
+	<form name="a">
+		<input type="hidden" name="1">
+		<input type="hidden" name="name" value='.$name.'>
+	</form>
 
-<form name="d">
-	<input type="hidden" name="2">
-</form>
+	<form name="d">
+		<input type="hidden" name="2">
+		<input type="hidden" name="name" value='.$name.'>
+	</form>
 
-<form name="s">
-	<input type="hidden" name="3">
-</form>
+	<form name="s">
+		<input type="hidden" name="3">
+		<input type="hidden" name="name" value='.$name.'>
+	</form>';
+	
 
-<?php
-	function drawRect($x, $y, $step)
+	function drawRect($x, $y, $step, $color)
 	{
 		$w = $step;
 		$h = $step;
 		
-		echo '<rect fill="red" width="'.$w.'" height="'.$h.'" x="'.($x - $w/2).'" y="'.($y - $h/2).'"/>';
+		echo '<rect fill="rgb(255,'.$color.','.$color.')" width="'.$w.'" height="'.$h.'" x="'.($x - $w/2).'" y="'.($y - $h/2).'"/>';
 	}
 ?>
 
 
 <svg width="98vw" height="98vh" style="border:2px solid black; background-color: pink">
 	<?php
-		foreach($player as $val)
-			if(isset($val['y']))
-				drawRect($val['x'], $val['y'], $step);
+		
+		foreach($temp_session as $players_ex)
+		{
+			if($players_ex == 'ball')
+				continue;
 			
+			$color = 0;
+			$color_step = 255 / (count($players_ex) - 1);
+			
+			foreach($players_ex as $val)
+				if(isset($val['y']))
+				{
+					$color += $color_step;
+					drawRect($val['x'], $val['y'], $step, $color);
+				}
+		}
+		
 		echo '<circle r='.($step*0.35).'px cx="'.$ball['x'].'" cy="'.$ball['y'].'" fill="blue">';
 	?>
 </svg>
@@ -126,22 +153,28 @@
 		//alert(keycode);
 		
 		if(keycode == 38)
-			w.submit()
+			w.submit();
 		
 		if(keycode == 37)
-			a.submit()
+			a.submit();
 		
 		if(keycode == 39)
-			d.submit()
+			d.submit();
 		
 		if(keycode == 40)
-			s.submit()
+			s.submit();
 	}
 </script>
 
 <?php
-	$_SESSION['ball'] = $ball;
-	$_SESSION['player'] = $player;
+	//session_id($mysession);
+	//session_start();
+	
+	session_id($global_session_id);
+	session_start();
+	$_SESSION = $temp_session;
 
-	header('Refresh: 0.1; url=game.php');
+	header('Refresh: 0.1; url=game.php?name='.$name);
 ?>
+
+
